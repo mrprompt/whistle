4a4bb72589c5debb9c74b36949f09ecd059f3891
Thiago Paes <mrprompt@gmail.com>
2016-11-18 02:25:43 -0200

    add pull up/down button

--- whistle.ino
+++ whistle.ino
@@ -3,23 +3,40 @@
  * 
  * @author Thiago Paes <mrprompt@gmail.com>
  */
-int buzzer = 9;  // conectar um buzzer ao pino 9
-unsigned int tom = 25000; // frequência
-unsigned long tempo = 500; // tempo
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+const int buzzerPin = 12;  // conectar um buzzer ao pino 12
+const int tom = 25000; // frequência para cães
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("Iniciando");
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+
+  // initialize the buzzer pin as an output:
+  pinMode(buzzerPin, OUTPUT);
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
-void loop()
-{
-  // play buzzer
-  tone(buzzer, tom, tempo);
-  delay(tempo);
-  noTone(buzzer);
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
 
-  // send status to serial
-  Serial.println(".");
+  // check if the pushbutton is pressed, if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+
+    // play buzzer
+    tone(buzzerPin, tom);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+
+    noTone(buzzerPin);
+  }
 }
